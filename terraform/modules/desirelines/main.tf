eaa116a1c53dd75d652f8e885b29bad81b489982
Andy Eschbacher <andyesch@protonmail.com>
2026-02-01 11:43:42 -0500

    refactor: reduce duplication

--- terraform/modules/desirelines/main.tf
+++ terraform/modules/desirelines/main.tf
@@ -229,14 +229,13 @@ resource "google_pubsub_topic_iam_member" "dead_letter_publisher" {
 # Grant PubSub service account permission to ack messages from source subscriptions
 # Required for dead letter forwarding: without this, DLQ delivery silently fails
 # and messages retry indefinitely instead of being forwarded after max_delivery_attempts
-resource "google_pubsub_subscription_iam_member" "bq_inserter_dlq_subscriber" {
-  subscription = google_pubsub_subscription.bq_inserter.name
-  role         = "roles/pubsub.subscriber"
-  member       = "serviceAccount:service-${var.gcp_project_number}@gcp-sa-pubsub.iam.gserviceaccount.com"
-}
+resource "google_pubsub_subscription_iam_member" "dlq_subscriber" {
+  for_each = toset([
+    google_pubsub_subscription.bq_inserter.name,
+    google_pubsub_subscription.postgres_writer.name,
+  ])
 
-resource "google_pubsub_subscription_iam_member" "postgres_writer_dlq_subscriber" {
-  subscription = google_pubsub_subscription.postgres_writer.name
+  subscription = each.value
   role         = "roles/pubsub.subscriber"
   member       = "serviceAccount:service-${var.gcp_project_number}@gcp-sa-pubsub.iam.gserviceaccount.com"
 }
